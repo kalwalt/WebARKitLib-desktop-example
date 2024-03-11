162bbbbe3f14558f6a23cd17b1f34039b8851d4d
kalwalt <github@kalwaltart.it>
2024-03-11 15:05:19 +0100

    minimal video capture example

--- main.cpp
+++ main.cpp
@@ -3,12 +3,15 @@
 #include <WebARKitTrackers/WebARKitOpticalTracking/WebARKitEnums.h>
 #include <WebARKitCamera.h>
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+
+void processVideo(webarkit::WebARKitManager& manager, cv::VideoCapture& capture);
 
 int main() {
     WEBARKIT_LOGi("Init WebARKit example...\n");
     webarkit::WebARKitManager manager;
-    // Init the manager with the Akaze tracker
-    manager.initialiseBase(webarkit::TRACKER_TYPE::AKAZE_TRACKER, 640, 480);
+    // Init the manager with the Teblid tracker
+    manager.initialiseBase(webarkit::TRACKER_TYPE::TEBLID_TRACKER, 640, 480);
      // Load the test image
     cv::Mat image = cv::imread("../pinball.jpg", cv::IMREAD_GRAYSCALE);
 
@@ -26,5 +29,40 @@ int main() {
 
     manager.initTracker(data, width, height, webarkit::ColorSpace::GRAY);
 
+    cv::VideoCapture cap;
+    processVideo(manager, cap);
+
     return 0;
+}
+
+void processVideo(webarkit::WebARKitManager& manager, cv::VideoCapture& capture)
+{
+    // Grab first frame to get the frame dimensions
+    cv::Mat currentFrame; 
+
+    capture.open(0); // Open the default camera
+
+    // Check the capture succeeded:
+    if (!capture.isOpened()){ // This section prompt an error message if no video stream is found//
+      std::cout << "No video stream detected" << std::endl;
+      system("pause");
+      return;
+    }
+
+    bool shouldQuit = false;
+    do
+    {
+        capture >> currentFrame;
+
+        cv::Size frameSize(currentFrame.cols, currentFrame.rows);
+
+        if (currentFrame.empty())
+        {
+            shouldQuit = true;
+            continue;
+        }
+
+        manager.processFrameData(currentFrame.data, currentFrame.cols, currentFrame.rows, webarkit::ColorSpace::RGBA);
+        shouldQuit = getKeyEsc();
+    } while (!shouldQuit);
 }
